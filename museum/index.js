71913221dc034f805171b7323f5492d9f99b6290
elygo <el.uzb93@gmail.com>
2021-09-27 17:31:12 +0500

    feat: add ticket-booking form

---
+++ museum/index.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("tickets__form").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("tickets__form").style.display = "none";
+  }
