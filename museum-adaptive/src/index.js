c807867962fbfde8fdeb4ee6e0d23373210ec145
elygo <el.uzb93@gmail.com>
2021-10-04 17:59:22 +0500

    feat: add webpack

---
+++ museum-adaptive/src/index.js
@@ -0,0 +1,29 @@
+import './style.css';
+
+function openForm() {
+    document.getElementById("tickets__form").style.display = "block";
+    document.getElementById("cover").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("tickets__form").style.display = "none";
+    document.getElementById("cover").style.display = "none";
+}
+
+
+
+  const navLink = document.querySelectorAll("#nav-link");
+  /* anchor links*/
+  let anchorlinks = document.querySelectorAll('a[href^="."]')
+  
+  for (let item of anchorlinks) { // relitere 
+      item.addEventListener('click', (e)=> {
+      let hashval = item.getAttribute('href')
+      let target = document.querySelector(hashval)
+      target.scrollIntoView({
+        behavior: 'smooth'
+      })
+      history.pushState(null, null, hashval)
+      e.preventDefault()
+    })
+  }
---
+++ museum-adaptive/webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+    context: path.resolve(__dirname, ''),
+    // devserver
+    target: "web",
+    //by default dev mode
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'public') 
+    },
+    // with this extensions are allowed to be omitted
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss'],
+        // alias for shortening the path
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/assets/favicon.ico'           
+        }),
+        new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, './src/assets/'),
+                        to: path.resolve(__dirname, './public/assets')
+                    }
+                ]
+            }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        overlay: true,
+        stats: 'errors-only',
+        clientLogLevel: 'none'
+      },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [ 
+                    'style-loader',
+                    'css-loader?url=false',
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [  
+                    {
+                        loader: "style-loader",
+                      },
+                      {
+                        loader: "css-loader",
+                      },
+                      {
+                        loader: "sass-loader",
+                        options: {
+                            
+                        },
+                      }
+                    ]
+            },
+            {
+                test: /\.less$/i,
+                use: [  
+                    {
+                        loader: "style-loader",
+                      },
+                      {
+                        loader: "css-loader",
+                      },
+                      {
+                        loader: "less-loader",
+                        options: {
+                          lessOptions: {
+                            javascriptEnabled: true
+                          },
+                        },
+                      }
+                    ]
+            },
+            {
+                test: /\.(svg|png|gif|jpg|ico)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: './public/assets/img/',
+                        publicPath:'./src/assets/img/'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+}
