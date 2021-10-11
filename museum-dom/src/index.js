b5cd33e921fe7423a1dc12c9c67224ec112c3af6
elygo <el.uzb93@gmail.com>
2021-10-11 19:04:44 +0500

    feat: add carousel, slider and map

--- museum-dom/src/index.js
+++ museum-dom/src/index.js
@@ -31,3 +31,26 @@ document.getElementById('closeButton').onclick =  function closeForm() {
       e.preventDefault()
     })
   }
+
+
+  //slider carousel
+  
+
+  //map
+
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiZWx5Z28iLCJhIjoiY2t1bWJwaG00MGQ0czJ2cGkzb2E2ejk5MyJ9.hHK_wg9p3siu4-QgOZmHmg";
+  const map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/mapbox/light-v10", // style URL
+    center: [2.335, 48.8605], // starting position [lng, lat]
+    zoom: 15, // starting zoom
+  });
+
+  map.addControl(new mapboxgl.NavigationControl());
+  const marker1 = new mapboxgl.Marker({color: "#858585"}).setLngLat([2.3364, 48.86091]).addTo(map);
+  const marker2 = new mapboxgl.Marker({color: "#858585"}).setLngLat([2.3333, 48.8602]).addTo(map);
+  const marker3 = new mapboxgl.Marker({color: "#858585"}).setLngLat([2.3397, 48.8607]).addTo(map);
+  const marker4 = new mapboxgl.Marker({color: "#858585"}).setLngLat([2.3330, 48.8619]).addTo(map);
+  const marker5 = new mapboxgl.Marker({color: "#858585"}).setLngLat([2.3365, 48.8625]).addTo(map);
+  
