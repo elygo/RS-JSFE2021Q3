a60d0f44b5e762246430cc102c5f61acc33e93ec
elygo <el.uzb93@gmail.com>
2021-12-01 09:57:27 +0500

    fix: add apiKey from newsapi.org

--- news-API/src/components/controller/appLoader.js
+++ news-API/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2889ed7825a84ddea353c14e7451245d', // получите свой ключ https://newsapi.org/
         });
     }
 }
