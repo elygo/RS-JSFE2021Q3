e74bd07d9781395d13cf516825961f049f1fcd56
elygo <el.uzb93@gmail.com>
2021-12-07 05:12:58 +0500

    feat: add alphabetic filter for news

--- news-API/src/components/view/sources/sources.ts
+++ news-API/src/components/view/sources/sources.ts
@@ -13,17 +13,40 @@ class Sources {
     public draw(data: Isource[]): void {
         const fragment = document.createDocumentFragment() as DocumentFragment;
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
-
-        data.forEach((item: Isource) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
-
-            (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
-            (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
-
-            fragment.append(sourceClone);
+        let c, button;
+        if ((document.querySelector('.alphabet') as HTMLElement).innerHTML == '') {
+            for (let i = 65; 90 >= i; i++) {
+                c = String.fromCharCode(i);
+                button = document.createElement('button') as HTMLElement;
+                button.className = 'name';
+                button.id = c;
+                button.innerHTML = c;
+                (document.querySelector('.alphabet') as HTMLElement).appendChild(button as HTMLElement);
+            }
+        }
+        // (document.querySelector('.name') as HTMLElement).addEventListener('click', () => {
+        //     console.log('aaa');
+        // });
+        //const res = document.querySelectorAll('.name');
+        document.addEventListener('click', function (e: MouseEvent) {
+            if ((e.target as Element).className == 'name') {
+                (document.querySelector('.sources') as HTMLElement).innerHTML = '';
+            }
+            if ((e.target as Element).className == 'name') {
+                data.forEach((item: Isource) => {
+                    const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+                    if (item.name.substring(0, 1).toLowerCase() === (e.target as Element).innerHTML.toLowerCase()) {
+                        (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+                        (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute(
+                            'data-source-id',
+                            item.id
+                        );
+                        fragment.append(sourceClone);
+                    }
+                    (document.querySelector('.sources') as HTMLElement).append(fragment);
+                });
+            }
         });
-
-        (document.querySelector('.sources') as HTMLElement).append(fragment);
     }
 }
 
