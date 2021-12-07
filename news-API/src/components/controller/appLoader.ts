ace272450cc8e0454bf27f4475111ebc659d3811
elygo <el.uzb93@gmail.com>
2021-12-07 07:02:38 +0500

    fix: change api-key (api limit reached)

--- news-API/src/components/controller/appLoader.ts
+++ news-API/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     public constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '2889ed7825a84ddea353c14e7451245d', // получите свой ключ https://newsapi.org/
+            apiKey: '68c4fc537e12420292167f524b0759de', // получите свой ключ https://newsapi.org/
         });
     }
 }
