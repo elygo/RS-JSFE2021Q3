e0da7d144b741995da5410f3bf262f00276d1f6c
elygo <el.uzb93@gmail.com>
2021-12-12 23:11:49 +0500

    fix: correct home page elements

--- christmas-task/src/components/header.ts
+++ christmas-task/src/components/header.ts
@@ -2,7 +2,6 @@ const Header = {
     render: async () => {
         return `
              <nav class="navbar" role="navigation" aria-label="main navigation">
-                <div class="container">
                     <div id="navbarBasicExample" class="navbar-menu is-active" aria-expanded="false">
                         <div class="navbar-start">
                             <a class="navbar-item" href="/#/">
@@ -16,7 +15,6 @@ const Header = {
                             </a>
                         </div>
                     </div>
-                </div>
             </nav>
         `;
     },
--- christmas-task/src/pages/home.ts
+++ christmas-task/src/pages/home.ts
@@ -5,10 +5,7 @@ const HomePage = {
             <div class="home__title-container">
                 <div class="home__title">Помогите бабушке нарядить елку</div>
             </div>
-            <div class="home__start-container">
-                <div class="home__start">Начать</div>
-            </div>
-            
+            <button class="home__start" onclick="location.href='/#/toys'" type="button">Начать</button>            
         </section>`;
     },
     after_render: async () => {},
