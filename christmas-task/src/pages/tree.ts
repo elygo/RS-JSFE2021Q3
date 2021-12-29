45b51f30835f4e9cae4a57e94c54fc7b34b16e61
elygo <el.uzb93@gmail.com>
2021-12-29 11:41:50 +0500

    feat: add complete drag`n`drop functionality

--- christmas-task/src/pages/tree.ts
+++ christmas-task/src/pages/tree.ts
@@ -1,25 +1,13 @@
 import { snowflakeFall } from '../utils/snowflake';
 import { createElement } from './toys';
 import data from '../data';
-// ------------------------ localstorage ------------------------//
 interface Itreepage {
     snow: string[];
     music: string[];
     tree: string[];
     bg: string[];
 }
-if (localStorage.getItem('treepage') === null) {
-    const treepage: Itreepage = {
-        snow: [],
-        music: [],
-        tree: [],
-        bg: [],
-    };
-
-    localStorage.setItem('treepage', JSON.stringify(treepage));
-}
-const treepage: Itreepage = JSON.parse(localStorage.getItem('treepage') as string);
-
+const treePageStorage: Itreepage = JSON.parse(localStorage.getItem('treepage') as string);
 // left part - trees container ----------------------------------//
 const selectTree = createElement('div', 'tree__select');
 // settings - sound and snowflake
@@ -106,9 +94,9 @@ function iterateTrees(): void {
     (document.getElementById('tree-picker') as HTMLElement).onclick = function (event: MouseEvent) {
         const target = event.target as HTMLImageElement;
         if (target.style.backgroundImage) {
-            treepage.tree.pop();
-            treepage.tree.push(target.style.backgroundImage.slice(20, -6).replace(/"/g, ''));
-            localStorage.setItem('treepage', JSON.stringify(treepage));
+            treePageStorage.tree.pop();
+            treePageStorage.tree.push(target.style.backgroundImage.slice(20, -6).replace(/"/g, '')); // change 20 to 19 before building
+            localStorage.setItem('treepage', JSON.stringify(treePageStorage));
             (document.getElementById(
                 'tree__arena--imagetree'
             ) as HTMLImageElement).src = target.style.backgroundImage.slice(4, -1).replace(/"/g, '');
@@ -120,7 +108,7 @@ function iterateBgs(): void {
     const bgElem = document.getElementById('bg-item') as HTMLElement;
     (document.getElementById('bg-picker') as HTMLElement).innerHTML = '';
     for (let k = 1; k <= 10; k++) {
-        bgElem.style.backgroundImage = "url('../assets/bg/" + k + ".jpg')";
+        bgElem.style.backgroundImage = "url('../assets/bg/" + k + ".jpg')"; // remove ../ before building
         (document.getElementById('bg-picker') as HTMLElement).innerHTML += bgElem.outerHTML;
     }
 
@@ -132,9 +120,9 @@ function iterateBgs(): void {
     (document.getElementById('bg-picker') as HTMLElement).onclick = function (event: MouseEvent) {
         const target = event.target as HTMLElement;
         if (target.style.backgroundImage) {
-            treepage.bg.pop();
-            treepage.bg.push(target.style.backgroundImage.slice(18, -6).replace(/"/g, ''));
-            localStorage.setItem('treepage', JSON.stringify(treepage));
+            treePageStorage.bg.pop();
+            treePageStorage.bg.push(target.style.backgroundImage.slice(18, -6).replace(/"/g, '')); // change 18 to 17 before building
+            localStorage.setItem('treepage', JSON.stringify(treePageStorage));
             (document.getElementById('arena') as HTMLElement).style.backgroundImage = target.style.backgroundImage;
         }
     };
@@ -167,8 +155,9 @@ function draggableToys(): void {
     });
 }
 
-function selectedToyDrag() {
+function selectedToyDrag(): void {
     const dragToysElem = document.getElementById('drag-toys-item') as HTMLElement;
+    dragToysElem.draggable = true;
     const dragToysImg = document.createElement('img') as HTMLImageElement;
     const dragToysQuant = document.createElement('p') as HTMLElement;
     (document.getElementById('drag-toys-container') as HTMLElement).innerHTML = '';
@@ -204,6 +193,18 @@ const TreePage = {
         return sectionsTree.outerHTML;
     },
     after_render: async (): Promise<void> => {
+        // ------------------------ localstorage ------------------------//
+        if (localStorage.getItem('treepage') === null) {
+            const treepage: Itreepage = {
+                snow: [],
+                music: [],
+                tree: [],
+                bg: [],
+            };
+
+            localStorage.setItem('treepage', JSON.stringify(treepage));
+        }
+        const treepage: Itreepage = JSON.parse(localStorage.getItem('treepage') as string);
         if ((document.getElementById('item-counter') as HTMLInputElement) && localStorage.getItem('select') !== null) {
             (document.getElementById('item-counter') as HTMLInputElement).innerHTML = JSON.parse(
                 localStorage.getItem('select') as string
@@ -261,7 +262,10 @@ const TreePage = {
         iterateTrees();
         iterateBgs();
         iterateGarland();
-        if (localStorage.getItem('select') === null) {
+        if (
+            localStorage.getItem('select') === null ||
+            JSON.parse(localStorage.getItem('select') as string).selected == ''
+        ) {
             draggableToys();
         } else {
             selectedToyDrag();
@@ -271,9 +275,32 @@ const TreePage = {
 
         for (let i = 0; i < dragToysIterate.length; i++) {
             (dragToysIterate[i] as HTMLImageElement).ondragstart = function drag(ev: DragEvent) {
+                // send an element
                 ev.dataTransfer?.setData('text/html', (ev.target as HTMLImageElement).id);
             };
+
+            (dragToysIterate[i] as HTMLElement).ondragover = function allowDrop(ev) {
+                ev.preventDefault();
+            };
+
+            (dragToysIterate[i] as HTMLElement).ondrop = function drop(ev) {
+                ev.preventDefault();
+                const data0 = ev.dataTransfer?.getData('text/html') as string;
+                const evTarget = document.getElementById(data0) as HTMLImageElement;
+                evTarget.style.left = '';
+                evTarget.style.top = '';
+                // count dragged in toys
+                (dragToysIterate[i] as HTMLElement).getElementsByTagName('p')[0].innerHTML = (
+                    Number((dragToysIterate[i] as HTMLElement).getElementsByTagName('p')[0].innerHTML) + 1
+                ).toString();
+                (dragToysIterate[i] as HTMLElement)?.append(evTarget);
+            };
         }
+
+        (document.getElementById('area') as HTMLElement).ondragstart = function drag(ev: DragEvent) {
+            // send an element
+            ev.dataTransfer?.setData('text/html', (ev.target as HTMLImageElement).id);
+        };
 
         (document.getElementById('area') as HTMLElement).ondragover = function allowDrop(ev) {
             ev.preventDefault();
@@ -285,6 +312,11 @@ const TreePage = {
             const evTarget = document.getElementById(data1) as HTMLImageElement;
             evTarget.style.left = ev.clientX - evTarget.offsetWidth / 2 + 'px';
             evTarget.style.top = ev.clientY - evTarget.offsetHeight + 'px';
+
+            // count dragged out from toys-div
+            (evTarget.parentNode as HTMLElement).getElementsByTagName('p')[0].innerHTML = (
+                Number((evTarget.parentNode as HTMLElement).getElementsByTagName('p')[0].innerHTML) - 1
+            ).toString();
             (ev.target as HTMLMapElement)?.append(evTarget);
         };
     },
