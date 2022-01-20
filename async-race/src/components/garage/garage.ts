7ed106a25ce7745b3d66b2c1e39de9387b04c40b
elygo <el.uzb93@gmail.com>
2022-01-20 15:16:02 +0500

    fix: apply try/catch for error handling and consistent code

--- async-race/src/components/garage/garage.ts
+++ async-race/src/components/garage/garage.ts
@@ -5,57 +5,79 @@ export interface ICars {
 }
 
 const baseApi = 'http://127.0.0.1:3000/';
+const carsPerPage = 7;
 class Garage {
     baseApi: string;
+    carsPerPage: number;
     constructor() {
         this.baseApi = baseApi;
+        this.carsPerPage = carsPerPage;
     }
 
     async getCars(url: string, page?: string) {
-        if (!page) page = '1';
-        const response = await fetch(url + `garage?_page=${page}&_limit=7`);
-        const data = await response.json();
-        localStorage.setItem('data', JSON.stringify(data));
+        try {
+            if (!page) page = '1';
+            const response = await fetch(url + `garage?_page=${page}&_limit=${this.carsPerPage}`);
+            const data: ICars[] = (await response.json()) || [];
+            localStorage.setItem('data', JSON.stringify(data));
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async createCar(url: string, name: string, color: string) {
-        await fetch(url + 'garage', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-
-            body: JSON.stringify({
-                name: name,
-                color: color,
-            }),
-        });
-        await this.getCars(this.baseApi);
+        try {
+            await fetch(url + 'garage', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: name,
+                    color: color,
+                }),
+            });
+            await this.getCars(this.baseApi);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async removeCar(url: string, id: string) {
-        await fetch(url + `garage/${id}`, {
-            method: 'DELETE',
-        }).then((res) => res.json());
+        try {
+            await fetch(url + `garage/${id}`, {
+                method: 'DELETE',
+            }).then((res) => res.json());
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async updateCar(url: string, id: string, name: string, color: string) {
-        await fetch(url + `garage/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: name,
-                color: color,
-            }),
-        });
+        try {
+            await fetch(url + `garage/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: name,
+                    color: color,
+                }),
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    async startstopCar(url: string, id: string, status: string) {
-        await fetch(url + `engine?id=${id}&status=${status}`, {
-            method: 'PATCH',
-        }).then((res) => res.json());
+    async startStopCar(url: string, id: string, status: string) {
+        try {
+            await fetch(url + `engine?id=${id}&status=${status}`, {
+                method: 'PATCH',
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
